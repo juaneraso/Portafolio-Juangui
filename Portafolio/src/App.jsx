90016750e67184ab820e61d65794b3f8a3a5f401
Juan <juangui9508@hotmail.com>
2023-12-27 16:52:36 -0500

    about

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -1,5 +1,6 @@
 
 import Header from "./components/Header/Header"
+import About from "./components/about/about"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header/>
+      <About/>
     </>
   )
 }
