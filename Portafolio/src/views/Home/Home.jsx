e88a385a6f91fe18788128ed856d3fe64e835819
Juan <juangui9508@hotmail.com>
2024-01-02 18:47:16 -0500

    fixing letters

--- Portafolio/src/views/Home/Home.jsx
+++ Portafolio/src/views/Home/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../../components/Header/Header";
 import About from "../../components/About/About";
 import Projects from "../../components/Projects/Projects";
 import Contact from "../../components/Contact/Contact";
-import Footer from "../../components/footer/footer";
+import Footer from "../../components/Footer/Footer";
 
 
 import style from "../Home/Home.module.css";
